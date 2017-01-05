c26967a5815fd6c72404fe2fd60efacae2050ba7
Christian Fehmer <cfe@sexy-developer.com>
2017-01-05 12:56:22 +0100

    v1.0.0 first public release

--- README.md
+++ README.md
@@ -5,4 +5,168 @@ Monitor travis-ci builds in your i3-status bar.
 [![npm version](https://badge.fury.io/js/i3-status-travis.svg)](https://badge.fury.io/js/i3-status-travis)
 [![Dependency Status](https://gemnasium.com/badges/github.com/fehmer/i3-status-travis.svg)](https://gemnasium.com/github.com/fehmer/i3-status-travis)
 [![Build Status](https://travis-ci.org/fehmer/i3-status-travis.svg?branch=master)](https://travis-ci.org/fehmer/i3-status-travis)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/59f026abe8ce42c28426e3728db24cf2)](https://www.codacy.com/app/fehmer/i3-status-travis?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=fehmer/i3-status-travis&amp;utm_campaign=Badge_Grade)
+[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/59f026abe8ce42c28426e3728db24cf2)](https://www.codacy.com/app/fehmer/i3-status-travis?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=fehmer/i3-status-travis&amp;utm_campaign=Badge_Coverage)
 
+This module for [i3-status](https://www.npmjs.com/package/i3-status) displays the build status from [Travis CI](https://travis-ci.org) projects. Projects without builds or pending or running builds will be ignored.
+
+If you use private builds with Travis Pro or Travis Enterprise you need to provide the config values for *url* and  *token*. This feature is currently untested. If it works please send me a note, thanks.
+
+
+## Table of content
+<!-- MarkdownTOC -->
+
+- [Installation](#installation)
+- [Example configurations](#example-configurations)
+  - [Showing builds for a single user](#showing-builds-for-a-single-user)
+  - [Show build status for multiple users](#show-build-status-for-multiple-users)
+  - [Show only selected projects](#show-only-selected-projects)
+- [Configuration values](#configuration-values)
+  - [url](#url)
+  - [token](#token)
+  - [user](#user)
+  - [project](#project)
+- [Modify the output](#modify-the-output)
+
+<!-- /MarkdownTOC -->
+
+
+## Installation
+
+``` sh
+cd ~/my-i3-status   # go to the directory you installed i3-status in
+npm install --save i3-status-travis
+```
+
+
+## Example configurations
+
+You need to provide at least one user to fetch to builds for. The user is the part of the travis ci url, e.g. ```user=fehmer``` will monitor all builds available at [https://travis-ci.org/fehmer/](https://travis-ci.org/fehmer/)
+
+### Showing builds for a single user
+
+This config will show the status of recent builds for one user.
+
+``` yaml
+  - name: travis
+    module: i3-status-travis
+    interval: 300 # refresh each 5 minutes
+    label: travis
+    user: fehmer
+```
+
+
+### Show build status for multiple users
+
+This config will show the status of recent builds for multiple users combined.
+
+
+``` yaml
+  - name: travis
+    module: i3-status-travis
+    interval: 300 # refresh each 5 minutes
+    label: travis
+    user: 
+     - fehmer
+     - nodejs
+```
+
+
+
+### Show only selected projects
+
+This config will show the status of the defined projects recent builds for one user. The project value format is ```<user>/<project>```
+
+
+``` yaml
+  - name: travis
+    module: i3-status-travis
+    interval: 300 # refresh each 5 minutes
+    label: travis
+    user: fehmer
+    project: fehmer/i3-status-travis
+```
+
+
+## Configuration values
+
+Common configuration values like label and interval are described in the [i3-status documentation](https://github.com/fehmer/i3-status/blob/master/docs/configuration.md)
+
+
+### url
+
+Provide the URL the [Travis CI API](https://docs.travis-ci.com/api#overview). For public accessible projects you do not need to provide an url and ```https://api.travis-ci.org``` is used.  If you use Travis Enterprise you have to provide the url of your travis server, e.g. ```https://travis.example.com/api```.
+
+The url should not end with a slash.
+
+
+### token
+
+If you use Travis Pro or Travis Enterprise you have to set this value. The [Travis API Documentation](https://docs.travis-ci.com/api#authentication) tells you what value to provide here.
+
+
+### user
+
+You can configure a single or multiple users. All available build states for each user is gathered from travis ci and combined for the output.
+
+For a single user just set the value like this:
+
+``` yaml
+  user: fehmer
+```
+
+
+For multiple users you can provide a list like this:
+
+
+``` yaml
+  user:
+    - fehmer
+    - nodejs
+```
+
+
+### project
+
+If you only want to monitor selected projects you can provide a single project name or multiple project names. A project name contains of the user and the project name, e.g. ```fehmer/i3-status-travis```.
+
+For a single project just set the project name like this:
+
+``` yaml
+  user: fehmer
+  project: fehmer/i3-status-travis
+```
+
+
+For multiple projects you can provide a list like this:
+
+``` yaml
+  user:
+    - fehmer
+    - nodejs
+  project:
+    - fehmer/i3-status-travis
+    - nodejs/nodejs.org
+```
+
+
+## Modify the output 
+
+When you activate the *colorize* option a successful build state is shown in green and a failed build state in red. 
+
+``` yaml
+    colorize: true
+```
+
+
+You can define the texts and colors for successful and failed builds.
+The default text is **** for successful and **** for failed builds.
+
+``` yaml
+    success:
+      color: '#AAFFAA'
+      text: all systems are go
+    failure:
+      color: '#FFAAAA'
+      text: hudson, we have a problem
+```
---
+++ test/index.js
@@ -0,0 +1,595 @@
+'use strict';
+
+import { expect } from 'chai';
+import server from 'mockyeah';
+import Travis from './../lib/index';
+import request from 'request';
+
+const options = {
+    url: 'http://localhost:4001',
+    user: 'fehmer',
+    colorize: true
+};
+
+describe('Travis Module', function() {
+    // remove service mocks after each test
+    afterEach(() => server.reset());
+
+    describe('#constructor', function() {
+        it('should construct with defaults', () => {
+            //construct block
+            var block = new Travis({
+                user: 'fehmer'
+            });
+
+            //expect url and user to be set
+            expect(block.url).to.equal('https://api.travis-ci.org');
+            expect(block.user).to.contain('fehmer');
+
+            //expect the status text to be the default values
+            expect(block.status.success).to.equal('');
+            expect(block.status.failure).to.equal('');
+
+            //expect color to be false
+            expect(block.color).to.be.false;
+        });
+
+        it('should construct with custom url', () => {
+            //construct block
+            var block = new Travis({
+                url: 'https://my.travis-server.tld',
+                user: 'peter'
+            });
+
+            //expect url to be set
+            expect(block.url).to.equal('https://my.travis-server.tld');
+        });
+
+        it('should construct with custom status', () => {
+            //use a config with custom status texts
+            var config = Object.assign({}, options, {
+                success: {
+                    text: 'OK'
+                },
+                failure: {
+                    text: 'DOOMED'
+                }
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //expect the custom status texts to be set
+            expect(block.status.success).to.equal('OK');
+            expect(block.status.failure).to.equal('DOOMED');
+        });
+
+        it('should construct with default colors if colorize is enabled', () => {
+            //enable colors in config
+            var config = Object.assign({}, options, {
+                colorize: true
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //check colors
+            expect(block.color.success).to.equal('#00FF00');
+            expect(block.color.failure).to.equal('#FF0000');
+        });
+
+        it('should construct with custom colors', () => {
+            //enable colors in config
+            var config = Object.assign({}, options, {
+                success: {
+                    color: '#88FF88'
+                },
+                failure: {
+                    color: '#FF8888'
+                }
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //check colors
+            expect(block.color.success).to.equal('#88FF88');
+            expect(block.color.failure).to.equal('#FF8888');
+        });
+
+        it('should fail without user', () => {
+
+            expect(function() {
+                var client = new Travis({});
+            }).to.throw('config value user is missing');
+        });
+
+        it('should fail with invalid user type', () => {
+            //enable project filter in config
+            var config = Object.assign({}, options, {
+                user: {
+                    test: 'test'
+                }
+            });
+
+            //construct block
+            expect(function() {
+                var client = new Travis(config);
+            }).to.throw('config value user must be a string or an array');
+        });
+
+
+        it('should construct with single project filter', () => {
+            //enable project filter in config
+            var config = Object.assign({}, options, {
+                project: 'i3-status-gitlab'
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //check project filter
+            expect(block.project).to.be.a('array');
+            expect(block.project).to.include('i3-status-gitlab');
+        });
+
+        it('should construct with single project filter', () => {
+            //enable project filter in config
+            var config = Object.assign({}, options, {
+                project: ['i3-status-gitlab', 'i3-status']
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //check project filter
+            expect(block.project).to.be.a('array');
+            expect(block.project).to.include('i3-status-gitlab');
+            expect(block.project).to.include('i3-status');
+        });
+
+        it('should fail with invalid project type', () => {
+            //enable project filter in config
+            var config = Object.assign({}, options, {
+                project: {
+                    test: 'test'
+                }
+            });
+
+            //construct block
+            expect(function() {
+                var client = new Travis(config);
+            }).to.throw('config value project must be a string or an array');
+        });
+
+
+        it('should construct with token', () => {
+            //enable project filter in config
+            var config = Object.assign({}, options, {
+                token: 'secretToken'
+            });
+
+            //construct block
+            var block = new Travis(config);
+
+            //check token
+            expect(block.token).to.equal('secretToken');
+        });
+
+    });
+
+
+    describe('update for all projects', function() {
+        it('should handle success', (done) => {
+
+            //construct block
+            var config = Object.assign({}, options);
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'passed'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: null
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: ''
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('');
+                expect(output.full_text).to.equal('');
+                expect(output.color).to.equal('#00FF00');
+
+                done();
+            });
+        });
+
+        it('should handle failures', (done) => {
+
+            //construct block
+            var config = Object.assign({}, options);
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'errored'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: null
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: ''
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('');
+                expect(output.full_text).to.equal('');
+                expect(output.color).to.equal('#FF0000');
+
+                done();
+            });
+        });
+
+        it('should handle multiple failures', (done) => {
+
+            //construct block
+            var config = Object.assign({}, options);
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'failed'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: 'errored'
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: ''
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal(' (2)');
+                expect(output.full_text).to.equal(' (2)');
+                expect(output.color).to.equal('#FF0000');
+
+                done();
+            });
+        });
+
+        it('should handle errors', (done) => {
+
+            //construct block
+            var config = Object.assign({}, options);
+            var block = new Travis(config, {});
+
+            //prepare mock response, responds error
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            });
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('Error: Got response code 404');
+                expect(output.full_text).to.equal('Error: Got response code 404');
+                expect(output.color).to.be.undefined;
+
+                done();
+            });
+        });
+
+        it('should handle invalid results', (done) => {
+
+            //construct block
+            var config = Object.assign({}, options);
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {});
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('result does not contain any repo data');
+                expect(output.full_text).to.equal('result does not contain any repo data');
+                expect(output.color).to.be.undefined;
+
+                done();
+            });
+        });
+
+    });
+
+
+    describe('update for filtered projects', function() {
+        it('should handle success', (done) => {
+
+            //construct block, filter two projects
+            var config = Object.assign({}, options, {
+                project: ['fehmer/i3-status-gitlab', 'fehmer/i3-status']
+            });
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'errored'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: null
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: ''
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('');
+                expect(output.full_text).to.equal('');
+
+                done();
+            });
+        });
+
+        it('should handle failures', (done) => {
+
+            //construct block, filter two projects
+            var config = Object.assign({}, options, {
+                project: ['fehmer/i3-status-gitlab', 'fehmer/i3-status']
+            });
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'errored'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: 'errored'
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: ''
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('');
+                expect(output.full_text).to.equal('');
+
+                done();
+            });
+        });
+
+
+        it('should handle multiple failures', (done) => {
+
+            //construct block, filter two projects
+            var config = Object.assign({}, options, {
+                project: ['fehmer/i3-status-gitlab', 'fehmer/i3-status']
+            });
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            }, {
+                repos: [{
+                    id: 1,
+                    slug: 'fehmer/i3-status-travis',
+                    last_build_state: 'errored'
+                }, {
+                    id: 2,
+                    slug: 'fehmer/i3-status-gitlab',
+                    last_build_state: 'errored'
+                }, {
+                    id: 3,
+                    slug: 'fehmer/i3-status',
+                    last_build_state: 'errored'
+                }]
+            });
+
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal(' (2)');
+                expect(output.full_text).to.equal(' (2)');
+
+                done();
+            });
+        });
+
+        it('should handle errors', (done) => {
+
+            //construct block, filter two projects
+            var config = Object.assign({}, options, {
+                project: ['fehmer/i3-status-gitlab', 'fehmer/i3-status']
+            });
+            var block = new Travis(config, {});
+
+            //prepare mock response, responds error
+            const expectation = mock('/repos/fehmer', {
+                active: 'true'
+            });
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('Error: Got response code 404');
+                expect(output.full_text).to.equal('Error: Got response code 404');
+
+                done();
+            });
+        });
+    });
+
+
+    describe('update with token', function() {
+        it('should handle success', (done) => {
+
+            //construct block, filter two projects
+            var config = Object.assign({}, options, {
+                token: 'mySecret'
+            });
+            var block = new Travis(config, {});
+
+            //prepare mock response
+            const expectation = server
+                .get('/repos/fehmer', {
+                    json: {
+                        repos: [{
+                            id: 1,
+                            slug: 'fehmer/i3-status-travis',
+                            last_build_state: 'passed'
+                        }, {
+                            id: 2,
+                            slug: 'fehmer/i3-status-gitlab',
+                            last_build_state: null
+                        }, {
+                            id: 3,
+                            slug: 'fehmer/i3-status',
+                            last_build_state: ''
+                        }]
+                    }
+                })
+                .expect()
+                .params({
+                    active: 'true'
+                })
+                .header('User-Agent', 'i3-status-travis/1.0.0')
+                .header('Accept', 'application/vnd.travis-ci.2+json')
+                .header('Authorization', 'token mySecret')
+                .once();
+
+            execute(block, (output) => {
+                //verify server interaction
+                expectation.verify();
+
+                //check output line
+                expect(output.short_text).to.equal('');
+                expect(output.full_text).to.equal('');
+
+                done();
+            });
+        });
+    });
+
+
+
+})
+
+
+//copied from i3-status
+function execute(block, verify) {
+    block.name = block.constructor.name;
+
+    block.on('updated', function(target, output) {
+        clearInterval(block.interval);
+
+        expect(target.name).to.equal(block.name);
+        verify(output);
+    });
+
+    //simulate set interval, will never fire
+    block._interval = 10000;
+    block.interval = setInterval(() => {
+        block.update();
+    }, block._interval);
+
+    block.update();
+}
+
+function mock(url, params, response) {
+    var result;
+
+    if (response) {
+        result = server.get(url, {
+            json: response
+        });
+    } else {
+        result = server.get(url, {
+            status: 404
+        });
+    }
+
+    return result
+        .expect()
+        .params(params)
+        .header('User-Agent', 'i3-status-travis/1.0.0')
+        .header('Accept', 'application/vnd.travis-ci.2+json')
+        .once();
+}
